139a1849d13603db523cfc383f1710cd0211a32d
Rhythem Sharma <rhythemvsharma@gmail.com>
2026-02-04 23:21:37 +0530

    trained YOLO for object detection and on tennis balls

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,13 +1,15 @@
 from ultralytics import YOLO
 
-# Load the YOLO model
-model = YOLO("yolov8x.pt")
+# Load the YOLO 
 
-results = model.predict('input_videos/input_video.mp4', save=True)
-print(results)
-print("boxes:")
-for box in results[0].boxes:
-    print(box)
+model = YOLO("yolov10n.pt")
+# model = YOLO("C:/Tennis Analysis/training/runs/detect/train2/weights/last.pt")
+
+results = model.track('input_videos/input_video.mp4',conf=0.2, save=True)
+# print(results)
+# print("boxes:")
+# for box in results[0].boxes:
+#     print(box)
 
 
 
