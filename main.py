ecc6fbca5c2e6b460e111e07553d8705e7581ecc
Rhythem Sharma <rhythemvsharma@gmail.com>
2026-02-14 00:01:42 +0530

    Implement player and ball tracking with YOLO, including stub-based detection and bounding box drawing on video frames.

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from utils import read_video, save_video
+from trackers import PlayerTracker, BallTracker
 
 def main():
+    # Read video
     input_video_path = "input_videos/input_video.mp4"
     video_frames = read_video(input_video_path)
 
-    save_video(video_frames, "output_videos/output_video.avi")
+    # Track players and balls
+    player_tracker = PlayerTracker("models/yolov8n.pt")
+    ball_tracker = BallTracker("models/yolov8n.pt")
+    player_detection = player_tracker.detect_frames(video_frames, read_from_stub=True, stub_path="tracker_stubs/player_detection.pkl")
+    ball_detection = ball_tracker.detect_frames(video_frames, read_from_stub=True, stub_path="tracker_stubs/ball_detection.pkl")
+
+    # Draw output
+
+    ## Draw bboxes
+    output_video_frames = player_tracker.draw_bboxes(video_frames, player_detection)
+    output_video_frames = ball_tracker.draw_bboxes(output_video_frames, ball_detection)
+    
+
+    save_video(output_video_frames, "output_videos/output_video.avi")
     
 
 if __name__ == "__main__":
--- trackers/player_tracker.py
+++ trackers/player_tracker.py
@@ -1,10 +1,56 @@
 from ultralytics import YOLO
+import cv2
+
+import pickle
+import sys
+import os
 
 class PlayerTracker:
     def __init__(self, model_path):
         self.model = YOLO(model_path)
 
-    def detect(self, frame):
-        results = self.model.track(frame, persist=True)
-        return results
+    def detect_frames(self, frames, read_from_stub=False, stub_path=None):
+        player_detection = []
+
+        if read_from_stub and stub_path is not None and os.path.exists(stub_path):
+            with open(stub_path, 'rb') as f:
+                player_detection = pickle.load(f)
+            return player_detection
+
+        for frame in frames:
+            player_dict = self.detect(frame)
+            player_detection.append(player_dict)
         
+        if stub_path is not None:
+            with open(stub_path, 'wb') as f:
+                pickle.dump(player_detection, f)
+        
+        return player_detection
+        
+
+    def detect(self, frame):
+        results = self.model.track(frame, persist=True)[0]
+        id_name_dict = results.names
+
+        player_dict = {}
+        for box in results.boxes:
+            track_id = int(box.id.tolist()[0])
+            result = box.xyxy.tolist()[0]
+            object_cls_id = box.cls.tolist()[0]
+            object_cls_name = id_name_dict[object_cls_id]
+            if object_cls_name == "person":
+                player_dict[track_id] = result
+        return player_dict
+
+    def draw_bboxes(self,video_frames, player_detection):
+        output_video_frames = []
+        for frame, player_dict in zip(video_frames, player_detection):
+            # Draw bboxes
+            for track_id, bbox in player_dict.items():
+                x1, y1, x2, y2 = bbox
+                cv2.putText(frame, f"Player ID: {track_id}", (int(bbox[0]), int(bbox[1] - 10)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 2)
+            output_video_frames.append(frame)
+            
+        return output_video_frames
+                
