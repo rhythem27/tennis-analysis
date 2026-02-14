3dcc9de5f906288adf368d8e8c83163151a6355c
Rhythem Sharma <rhythemvsharma@gmail.com>
2026-02-14 13:22:48 +0530

    Implement court line detection and drawing by adding a new model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils import read_video, save_video
 from trackers import PlayerTracker, BallTracker
+from court_line_detector import CourtLineDetector
 
 def main():
     # Read video
@@ -12,11 +13,19 @@ def main():
     player_detection = player_tracker.detect_frames(video_frames, read_from_stub=True, stub_path="tracker_stubs/player_detection.pkl")
     ball_detection = ball_tracker.detect_frames(video_frames, read_from_stub=True, stub_path="tracker_stubs/ball_detection.pkl")
 
+    # Detect court lines
+    court_model_path = "training/tennis_court_keypoint.pt"
+    court_line_detector = CourtLineDetector(model_path=court_model_path)
+    court_keypoints = court_line_detector.predict(video_frames[0])
+
     # Draw output
 
     ## Draw bboxes
     output_video_frames = player_tracker.draw_bboxes(video_frames, player_detection)
     output_video_frames = ball_tracker.draw_bboxes(output_video_frames, ball_detection)
+
+    ## Draw court lines
+    output_video_frames = court_line_detector.keypoints_on_video(output_video_frames, court_keypoints)
     
 
     save_video(output_video_frames, "output_videos/output_video.avi")
